be3bd974b1005183ee4f4a2f6adcbfe955dd2f80
Denise Costa <denisecosta.codes@gmail.com>
2023-08-03 15:36:40 -0300

    Feat: Atribui o desenvolvimento das tabelas aos respectivos colaboradores

---
+++ scripts SQL/data_resilia.sql
@@ -0,0 +1,11 @@
+-- Inserts tabela estudantes - Dilnae
+
+-- Inserts tabela pessoas facilitadoras - Denise
+
+-- Inserts tabela turmas - José
+
+-- Inserts tabela módulos - Em grupo
+
+-- Inserts tabela cursos - Tawany
+
+-- Inserts tabela categorias de curso(extra) - Denise
---
+++ scripts SQL/db_resilia.sql
@@ -0,0 +1,13 @@
+-- Criação do banco de dados
+
+-- Tabela estudantes - Dilnae
+
+-- Tabela pessoas facilitadoras - Denise
+
+-- Tabela turmas - José
+
+-- Tabela módulos - Em grupo
+
+-- Tabela cursos - Tawany
+
+-- Tabela categorias de curso(extra) - Denise
