7f542f24ffa4137d9510efeb1eeeb8dad1f15924
Taw Simões <tawm.dev@gmail.com>
2023-08-07 00:57:09 -0300

    Adiciona mais seis registros de nomes de cursos da Resilia

--- scripts SQL/data_resilia.sql
+++ scripts SQL/data_resilia.sql
@@ -1,18 +1,21 @@
 -- Inserts tabela estudantes - Dilnae
-
 -- Inserts tabela pessoas facilitadoras - Denise
-
 -- Inserts tabela turmas - José
-
 -- Inserts tabela módulos - Em grupo
-
 -- Inserts tabela cursos - Tawany
+INSERT INTO
+    curso (nome, carga_horaria);
 
-INSERT INTO curso (nome, carga_horaria);
 VALUES
-('Formação em Análise de Sistemas'),
-('Formação em Data Analytics'),
-('Formação em WebDev Full Stack Java'),
-('Formação em WebDev Full Stack JavaScript');
+    ('Formação em Análise de Sistemas'),
+    ('Formação em Data Analytics'),
+    ('Formação em Hacker de Negócios'),
+    ('Formação em Iniciação em Cultura Digital'),
+    ('Formação em Upskilling e Reskilling'),
+    ('Formação em WebDev Full Stack Java'),
+    ('Formação em WebDev Full Stack JavaScript'),
+    ('Programa de Estágio Resilia'),
+    ('Programa de Liderança Inovadora'),
+    ('Programa de Trainee Resilia');
 
 -- Inserts tabela categorias de curso(extra) - Denise
