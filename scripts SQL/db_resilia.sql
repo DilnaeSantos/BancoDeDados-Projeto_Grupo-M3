645b08a67212a32d4d4c1b1f810c1e351c89d5cd
Taw Simões <tawm.dev@gmail.com>
2023-08-08 11:07:25 -0300

    FEAT: Adiciona instrução que define o conjunto de caracteres padrão que será usado ao criar tabelas / colunas.

--- scripts SQL/db_resilia.sql
+++ scripts SQL/db_resilia.sql
@@ -3,12 +3,13 @@
 -- Tabela pessoas facilitadoras - Denise
 -- Tabela turmas - José
 -- Tabela módulos - Em grupo
+
 -- Tabela cursos - Tawany
 CREATE TABLE
     `curso` (
         `id_curso` int PRIMARY KEY AUTO_INCREMENT,
         `nome` varchar(100) NOT NULL,
         `carga_horaria` int
-    );
+) default charset = utf8mb4;
 
 -- Tabela categorias de curso(extra) - Denise
