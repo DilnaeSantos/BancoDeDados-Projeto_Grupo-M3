47e9d00d5d5fb59f5caf3c6267ed3685f74a249f
JojoPrc <joseproenca2005@gmail.com>
2023-08-11 00:06:55 -0300

    fix: renomeia  a pasta scripts sql para scripts

---
+++ scripts/criando_database.sql
@@ -0,0 +1,4 @@
+-- Cria o banco de dados
+CREATE DATABASE IF NOT EXISTS resilientes
+DEFAULT CHARACTER SET UTF8MB4 -- Define UTF-8 como padrão de codificação de caracteres
+DEFAULT COLLATE utf8mb4_general_ci;
