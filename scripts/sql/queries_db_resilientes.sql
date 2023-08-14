12b10fdb92b4cbff696776b29c31b881f16444ef
Taw Simões <tawm.dev@gmail.com>
2023-08-14 17:17:17 -0300

    FEAT: Adiciona os comentários referente as consultas obrigatórias do projeto

---
+++ scripts/sql/queries_db_resilientes.sql
@@ -0,0 +1,21 @@
+-- Seleciona o BANCO DE DADOS:
+USE resilientes;
+
+-- Consultas obrigatórias:
+
+-- 1. Seleciona a quantidade total de ALUNOS cadastrados no BANCO DE DADOS:
+SELECT COUNT(*) AS TotalAlunos
+FROM Aluno;
+
+-- 2. Seleciona quais FACILITADORES atuam em mais de uma TURMA:
+
+-- 3. Cria uma view que seleciona a % de ALUNOS com STATUS "evadido" agrupados por TURMA:
+
+-- 4. Insere um novo dado na tabela de LOG quando o atributo STATUS de um ALUNO é atualizado:
+    -- Os dados foram inseridos manualmente no "INSERT INTO log".
+
+-- 
+
+
+
+
