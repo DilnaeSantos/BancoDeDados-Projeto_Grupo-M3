b3e637fd7efc45b81b6edef8067cbd3dde545972
Taw Simões <tawm.dev@gmail.com>
2023-08-14 20:21:07 -0300

    FIX: Faz a ordenação correta dos comentários referente às consultas.

--- scripts/sql/queries_db_resilientes.sql
+++ scripts/sql/queries_db_resilientes.sql
@@ -7,41 +7,47 @@ SELECT COUNT(*) AS total_alunos
 FROM aluno;
 
 -- 2. Seleciona quais FACILITADORES atuam em mais de uma TURMA:
-SELECT cmf.id_facilitador, f.nome, COUNT(DISTINCT cmf.id_curso) AS total_cursos
-FROM curso_modulo_facilitador cmf
-JOIN facilitador f ON cmf.id_facilitador = f.id_facilitador
-GROUP BY cmf.id_facilitador
-HAVING COUNT(DISTINCT cmf.id_curso) > 1;
 
 -- 3. Cria uma view que seleciona a % de ALUNOS com STATUS "Evadido" agrupados por TURMA:
 
 -- 4. Insere um novo dado na tabela de LOG quando o atributo STATUS de um ALUNO é atualizado:
     -- Os dados foram inseridos manualmente no "INSERT INTO log".
 
--- Consultas extras:
-    -- Tema: Alunos PcD
--- 5. Cria uma view que selecione a % de ALUNOS com STATUS "Concluído" agrupados por ALUNOS PcD e por CURSO:
+-- Consulta obrigatória que combina pelo menos 3 tabelas:
+-- 1. Seleciona os FACILITADORES que estão ativos, mas estão disponíveis para novas TURMAS:
 
--- 6. Seleciona a quantidade total de ALUNOS PcD cadastrados no BANCO DE DADOS:
+
+-- Consultas extras | Tema - Alunos PcD:
+-- 1. Seleciona a quantidade total de ALUNOS PcD cadastrados no BANCO DE DADOS:
 SELECT COUNT(*) AS total_alunos_pcd
 FROM aluno
 WHERE pcd = 1;
 
--- 7. Quantos ALUNOS PcD por curso?
-
--- 8. Quantos ALUNOS PcD por turma?
-
--- 9. Quantos ALUNOS PcD com status evadido?
-
--- 10. Quantos ALUNOS PcD com status ativo?
-
--- 11. Quantos ALUNOS PcD com status inativo?
-
--- 12. Quantos ALUNOS PcD com status reprovado?
-
--- 13. Qual o principal Estado de origem dos ALUNOS PcD?
-
--- 14. Calcula a média de idade dos ALUNOS PcD cadastrados no BANCO DE DADOS?
+-- 2. Calcula a média de idade dos ALUNOS PcD cadastrados no BANCO DE DADOS:
 SELECT ROUND(AVG(DATEDIFF(CURRENT_DATE, data_nascimento) / 365)) AS media_idade_alunos_pcd
 FROM aluno
 WHERE pcd = 1;
+
+-- 3. Qual o principal Estado de origem dos ALUNOS PcD?
+
+-- 4. Cria uma view que seleciona a quantidade de ALUNOS PcD agrupados por CURSO:
+CREATE VIEW view_alunos_pcd_por_curso AS
+SELECT curso.nome, COUNT(*) AS quantidade_alunos_pcd
+FROM aluno
+INNER JOIN turma ON aluno.id_turma = turma.id_turma
+INNER JOIN curso ON turma.id_curso = curso.id_curso
+WHERE aluno.pcd = 1
+GROUP BY curso.nome;
+
+-- 5. Quantos ALUNOS PcD por turma?
+
+-- 6. Quantos ALUNOS PcD com status ativo?
+
+-- 7. Quantos ALUNOS PcD com status inativo?
+
+-- 8. Quantos ALUNOS PcD com status evadido?
+
+-- 9. Quantos ALUNOS PcD com status reprovado?
+
+-- 10. Quantos com status concluído?
+
