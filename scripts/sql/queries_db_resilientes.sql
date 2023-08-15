8d963a678bddfb25ec7831d7d4b99b7559d1066e
Taw Simões <tawm.dev@gmail.com>
2023-08-15 11:34:58 -0300

    FEAT: Adiciona consulta que agrupa os alunos PcD por estado de origem.

--- scripts/sql/queries_db_resilientes.sql
+++ scripts/sql/queries_db_resilientes.sql
@@ -15,7 +15,6 @@ GROUP BY facilitador.id_facilitador, facilitador.nome
 HAVING COUNT(DISTINCT turma.id_turma) > 1;
 
 -- 3. Cria uma view que seleciona a % de ALUNOS com STATUS "Evadido" agrupados por TURMA:
-
 CREATE VIEW view_porcentagem_evasao AS
 SELECT
 t.nome AS nome_turma,
@@ -61,7 +60,15 @@ SELECT ROUND(AVG(DATEDIFF(CURRENT_DATE, data_nascimento) / 365)) AS media_idade_
 FROM aluno
 WHERE pcd = 1;
 
--- 3. Qual o principal Estado de origem dos ALUNOS PcD?
+-- 3. Cria uma view que seleciona a quantidade de ALUNOS PcD agrupados por estado de origem e organiza o resultado em ordem decrescente:
+CREATE VIEW view_alunos_pcd_por_estado AS
+SELECT endereco.estado, COUNT(*) AS quantidade_alunos_pcd
+FROM aluno
+INNER JOIN endereco ON aluno.id_endereco = endereco.id_endereco
+WHERE aluno.pcd = 1
+GROUP BY endereco.estado
+ORDER BY quantidade_alunos_pcd DESC;
+
 
 -- 4. Cria uma view que seleciona a quantidade de ALUNOS PcD agrupados por CURSO:
 CREATE VIEW view_alunos_pcd_por_curso AS
@@ -75,7 +82,6 @@ GROUP BY curso.nome;
 -- 5. Quantos ALUNOS PcD por turma?
 
 -- 6. Quantos ALUNOS PcD com status ativo?
-
 SELECT
 COUNT(*) AS total_alunos,
 SUM(CASE WHEN a.pcd = TRUE AND s.situacao = 'Ativo' THEN 1 ELSE 0 END) AS alunos_pcd_ativos,
@@ -85,7 +91,6 @@ JOIN status s ON a.id_status = s.id_status;
 
 
 -- 7. Quantos ALUNOS PcD com status inativo?
-
 SELECT
 COUNT(*) AS total_alunos,
 SUM(CASE WHEN a.pcd = TRUE AND s.situacao = 'Inativo' THEN 1 ELSE 0 END) AS alunos_pcd_inativos,
@@ -108,15 +113,14 @@ GROUP BY turma.id_turma, turma.nome;
 
 
 -- 9. Quantos ALUNOS PcD com status reprovado?
-
 SELECT
 COUNT(*) AS total_alunos,
 SUM(CASE WHEN a.pcd = TRUE AND s.situacao = 'Reprovado' THEN 1 ELSE 0 END) AS alunos_pcd_reprovados,
 ROUND((SUM(CASE WHEN a.pcd = TRUE AND s.situacao = 'Reprovado' THEN 1 ELSE 0 END) / COUNT(*)) * 100, 2) AS percentual_pcd_reprovados
 FROM aluno a
 JOIN status s ON a.id_status = s.id_status;
--- 10. Quantos com status concluído?
 
+-- 10. Quantos com status concluído?
 SELECT
 COUNT(*) AS total_alunos,
 SUM(CASE WHEN a.pcd = TRUE AND s.situacao = 'Concluído' THEN 1 ELSE 0 END) AS alunos_pcd_concluidos,
