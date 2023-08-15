0402000fe4d1dc630e03516d3f8c9167d6201bd7
JojoPrc <joseproenca2005@gmail.com>
2023-08-14 23:04:28 -0300

    fix:  adiciona duas casas  decimais

--- scripts/sql/queries_db_resilientes.sql
+++ scripts/sql/queries_db_resilientes.sql
@@ -106,10 +106,9 @@ HAVING COUNT(DISTINCT turma.id_turma) > 1;
 SELECT
 COUNT(*) AS total_alunos,
 SUM(CASE WHEN a.pcd = TRUE AND s.situacao = 'Reprovado' THEN 1 ELSE 0 END) AS alunos_pcd_reprovados,
-(SUM(CASE WHEN a.pcd = TRUE AND s.situacao = 'Reprovado' THEN 1 ELSE 0 END) / COUNT(*)) * 100 AS percentual_pcd_reprovados
+ROUND((SUM(CASE WHEN a.pcd = TRUE AND s.situacao = 'Reprovado' THEN 1 ELSE 0 END) / COUNT(*)) * 100, 2) AS percentual_pcd_reprovados
 FROM aluno a
 JOIN status s ON a.id_status = s.id_status;
-
 -- 10. Quantos com status concluído?
 
 SELECT
